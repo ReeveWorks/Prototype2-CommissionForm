8916bc1b726fc1fa5df74814a1ae8504d2ec177d
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-09-07 13:34:54 +0800

    v0.1 Main Page | Day 1
    Setting up main page

---
+++ ProtoForms/src/components/mainPage/mainPage.jsx
@@ -0,0 +1,20 @@
+//styles
+import './mainPage.css'
+
+//components
+
+//functional component
+import { useState } from 'react'
+
+
+function mainPage() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Hello World
+    </>
+  )
+}
+
+export default mainPage
