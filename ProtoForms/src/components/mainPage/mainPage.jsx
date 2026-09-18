52866b3e405d266671b6eaed741745836ef04bd0
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-09-18 09:17:29 +0800

    v0.1 Main Page | Day 12

--- ProtoForms/src/components/mainPage/mainPage.jsx
+++ ProtoForms/src/components/mainPage/mainPage.jsx
@@ -14,130 +14,94 @@ function mainPage() {
     <>
       <div className='mainPage'>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 1</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 2</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 3</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 4</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 5</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 6</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 7</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 8</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 9</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 10</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 11</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 12</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 13</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 14</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 15</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 16</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 17</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 18</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 19</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 20</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 21</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 22</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 23</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 24</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 25</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 26</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 27</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 28</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
+        <p>dummy text 29</p>
         <br></br>
         <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
-        <br></br>
-        <br></br>
-        <p>dummy text</p>
+        <p>dummy text 30</p>
         <br></br>
       </div>
     </>
