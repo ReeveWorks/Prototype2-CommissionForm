8d94eac7ab70d23218c6bc12e06e420cbb441c48
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-09-20 16:55:07 +0800

    v0.1 Main Page | Day 14

--- ProtoForms/src/components/footer/footer.jsx
+++ ProtoForms/src/components/footer/footer.jsx
@@ -2,10 +2,17 @@
 import './footer.css'
 
 function Footer() {
+  let value = window.scrollY;
+
+  const handleScroll = () => {
+    value = window.scrollY;
+    console.log(value);
+  }
 
   return (
     <div className='footer'>
       Footer
+      <button onClick={handleScroll}>Check Value</button>
     </div>
   )
 }
